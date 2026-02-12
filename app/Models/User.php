ec7dcbe27ef0ca2d27020ae9844060336b49259a
Administrator <821280918aa@gmail.com>
2026-02-12 17:19:07 +0800

    filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;K
+    use HasFactory, Notifiable;
 
     use HasRoles;
 
