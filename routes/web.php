c6ba8e055104a63931e1e678f5ba2c3010e5ce07
Administrator <821280918aa@gmail.com>
2026-02-12 16:31:24 +0800

    filament

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('/test', function () {
+//    \App\Models\User::create([
+//        'name' => 'test',
+//        'email' => 'test@test.com',
+//        'password' => bcrypt('123456'),
+//    ]);
+//
+//    return 'ok';
+//});
