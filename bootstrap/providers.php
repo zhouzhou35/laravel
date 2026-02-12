bfa2dac8449c1439bc21e314b72dad75c3305df4
Administrator <821280918aa@gmail.com>
2026-02-12 17:24:58 +0800

    filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-<<<<<<< HEAD
     App\Providers\Filament\AdminPanelProvider::class,
-=======
->>>>>>> 633ea8654c097225db98d01595d32307691008d5
+
 ];
